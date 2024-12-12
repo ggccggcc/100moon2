382b08960976d22fc3c62f90a737046731e670ac
dongwoo.kim <dongwoo.kim@nm-neo.com>
2024-12-12 17:39:23 +0900

    폴더별로 분류 확인용 커밋

---
+++ RoadBook.CsharpBasic.Chapter01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RoadBook.CsharpBasic.Chapter01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ RoadBook.CsharpBasic.Chapter02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RoadBook.CsharpBasic.Chapter02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
