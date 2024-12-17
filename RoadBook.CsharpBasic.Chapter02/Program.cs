8b13444d780d80ce4cafe1616f366a0f8cc34a82
dongwoo.kim <dongwoo.kim@nm-neo.com>
2024-12-17 16:33:22 +0900

    20241217_2챕터_변수등등

--- RoadBook.CsharpBasic.Chapter02/Program.cs
+++ RoadBook.CsharpBasic.Chapter02/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Examples.Ex003 test = new Examples.Ex003();
+            test.Run();
         }
     }
 }
