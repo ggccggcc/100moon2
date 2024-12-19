3a81d50aa02eda63b7ad4130b7df31be512053a4
dongwoo.kim <dongwoo.kim@nm-neo.com>
2024-12-19 18:31:44 +0900

    챕터3 예제 시작

---
+++ 100moon2/RoadBook.CsharpBasic.Chapter03/Examples/Ex001.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter03.Examples
+{
+    public class Ex001
+    {
+        public void Run()
+        {
+            const int zero = 0;
+
+            if (zero == 0)
+            {
+                Console.WriteLine("첫번째 if문입니다.");
+
+                if (zero == 0)
+                {
+                    Console.WriteLine("두번째 if문입니다.");
+                    Console.WriteLine("zero가 0이기에 출력합니다.");
+                }
+
+                if (zero == 1)
+                    Console.WriteLine("세번째 if문입니다.");
+                    Console.WriteLine("zero는 1이 아니라 출력에서 제외합니다.");
+            }
+
+        }
+    }
+}
---
+++ 100moon2/RoadBook.CsharpBasic.Chapter03/Program.cs
@@ -0,0 +1,13 @@
+﻿using RoadBook.CsharpBasic.Chapter03.Examples;
+
+namespace RoadBook.CsharpBasic.Chapter03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Examples.Ex001 test = new Examples.Ex001();
+            test.Run();
+        }
+    }
+}
