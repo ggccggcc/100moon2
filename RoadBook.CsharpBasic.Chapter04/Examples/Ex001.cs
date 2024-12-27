97fab42fe61177785e29470faf0410ca676bc80d
dongwoo.kim <dongwoo.kim@nm-neo.com>
2024-12-27 18:44:14 +0900

    반복문 초반

---
+++ RoadBook.CsharpBasic.Chapter04/Examples/Ex001.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadBook.CsharpBasic.Chapter04.Examples
+{
+    public class Ex001
+    {
+        public void Run()
+        {
+            const int number = 2;
+            int index = 1;
+
+            for (; ; )
+            {
+                Console.WriteLine("{0} * {1} = {2}", number, index, number * index);
+                }
+
+        }
+    }
+}
---
+++ RoadBook.CsharpBasic.Chapter04/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RoadBook.CsharpBasic.Chapter04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Examples.Ex002 test = new Examples.Ex002();
+            test.Run();
+        }
+    }
+}
